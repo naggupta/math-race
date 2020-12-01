652e56893fc427912697c981873230b2837be9a1
naggupta <31886022+naggupta@users.noreply.github.com>
2020-12-01 17:08:34 +0000

    Updates

---
+++ precache-manifest.a36ad15d02f86bf44824f769c2505233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f291df164cb7180e76003ab0e202dc01",
+    "url": "/math-race/index.html"
+  },
+  {
+    "revision": "c1415703c41e27a3c09b",
+    "url": "/math-race/static/css/main.20a2a965.chunk.css"
+  },
+  {
+    "revision": "e6303f74b7585e8f404c",
+    "url": "/math-race/static/js/2.bbdb519e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/math-race/static/js/2.bbdb519e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1415703c41e27a3c09b",
+    "url": "/math-race/static/js/main.7f25aa90.chunk.js"
+  },
+  {
+    "revision": "142626078afa8a64bc26",
+    "url": "/math-race/static/js/runtime-main.960fb439.js"
+  }
+]);
