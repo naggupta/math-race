b398474d23c78970682926a77fc395a203bc4a69
naggupta <31886022+naggupta@users.noreply.github.com>
2020-12-02 10:14:08 +0000

    Updates

--- precache-manifest.3dc7ac95476d64af99f460424382fba4.js
+++ precache-manifest.3dc7ac95476d64af99f460424382fba4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23ead241885acb72446b36d9b0d7d05d",
+    "revision": "c22b0d018323526f54dec9e00cb93ada",
     "url": "/math-race/index.html"
   },
   {
-    "revision": "97caa19e60b87a68c43a",
-    "url": "/math-race/static/css/main.8b9998cf.chunk.css"
+    "revision": "42ee4a9f16eb7d3c1204",
+    "url": "/math-race/static/css/main.f94a28f0.chunk.css"
   },
   {
     "revision": "9108f811373ca66553fd",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/math-race/static/js/2.cadafec8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "97caa19e60b87a68c43a",
-    "url": "/math-race/static/js/main.5fd1de21.chunk.js"
+    "revision": "42ee4a9f16eb7d3c1204",
+    "url": "/math-race/static/js/main.e199636b.chunk.js"
   },
   {
     "revision": "142626078afa8a64bc26",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/math-race/precache-manifest.d543d88f9cdd52442d13fa22f60504f4.js"
+  "/math-race/precache-manifest.3dc7ac95476d64af99f460424382fba4.js"
 );
 
 self.addEventListener('message', (event) => {
